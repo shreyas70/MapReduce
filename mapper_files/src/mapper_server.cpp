917fbdac99942d8a802d47ed51904f4b35b2ac05
Tarun <tarun.mohandas@gmail.com>
2018-11-25 02:54:53 +0530

    mapper server gets ip from command line

--- mapper_files/src/mapper_server.cpp
+++ mapper_files/src/mapper_server.cpp
@@ -4,15 +4,21 @@
 #include "MapperServer.h"
 using namespace std;
 
-int main()
+int main(int argc, char * argv[])
 {
+    if(argc<2)
+    {
+        cout<<"\nAre you insane?\n";
+        exit(1);
+    }
     vector<string> reducer_ips;
     reducer_ips.push_back("10.1.1.1:1000");
     reducer_ips.push_back("10.1.1.2:1000");
     reducer_ips.push_back("10.1.1.3:1000");
     reducer_ips.push_back("10.1.1.4:1000");
     MapperServer ms;
-    ms.set_mapper_ip("127.0.0.1:7000");
+    string mapper_ip = argv[1];
+    ms.set_mapper_ip(mapper_ip);
     ms.set_reducer_info(reducer_ips);
     ms.start_mapper_server();
     return 0;
