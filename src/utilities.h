9d844061dc2e5a570deb49d151ee9512889572ce
hiteshkaushik28 <hiteshkaushik545@gmail.com>
2018-12-02 03:27:49 +0530

    Added utility functions

--- src/utilities.h
+++ src/utilities.h
@@ -2,30 +2,17 @@
 #define _UTILITIES_H_
 
 #include <string>
+#include <fstream>
+#include <sys/stat.h>
 #include <vector>
-
-//#include <unistd.h>
-//#include <termios.h>
-//#include <dirent.h>
-//#include <sys/ioctl.h>
-//#include <sys/stat.h>
-
-//#include <sstream>         // stringstream
-//#include <fstream>
-//#include <list>
-//#include <stack>
-//#include <string>
-//#include <string.h>
-//#include <iostream>
-//#include <ftw.h>
-//#include <limits>
-//#include <cstdint>
 
 #define pb        push_back
 #define INT_MAX  (std::numeric_limits<std::int32_t>::max())
 
-#define FAILURE   -1
-#define SUCCESS   0
+#define FAILURE         -1
+#define SUCCESS         0
+#define INVALID_SOCK    -1
+#define MAX_CHUNK_SIZE  (512 * 1024)
 
 enum class Problem
 {
@@ -51,6 +38,12 @@ int                       util_connection_make(std::string ip, uint16_t port);
 void                      util_data_read(int sock, char* read_buffer, int size_to_read);
 int                       util_socket_data_get(int sock, std::string& buffer_str, std::string& error_msg);
 void                      util_write_to_sock(int sock, std::string data);
+void                      util_file_data_send(int sock, std::string filename, long pos, int remaining_bytes);
+int                       util_file_data_read(std::ifstream &inFile, int chunk_size, std::string& buffer_str);
+size_t                    util_file_size_get(std::string filename);
+bool                      util_file_exists(std::string filename);
+void                      util_read_data_into_file(int m_file_socket, std::string filename);
+void                      util_complete_file_data_send(int sock, std::string filename);
 std::vector<std::string>  split_string(std::string input_string, char delimiter);
 
 #endif
